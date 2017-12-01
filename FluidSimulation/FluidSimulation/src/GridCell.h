312a7b0848b08917cdb416142bcd05cb1b818455
Balakumaran Palanivel <kumaraxe@gmail.com>
2017-12-01 11:26:07 +0000

    [INTEG] Adding GridCell definition which contains a number of GridPoints

---
+++ FluidSimulation/FluidSimulation/src/GridCell.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+#include <glm\glm.hpp>
+#include "GridPoint.h"
+
+class CGridCell
+{
+public:
+	CGridCell();
+	void Reset();
+	void Initialize(int i, int j, int k);
+	void InsertGridPoint(GridPoint* p);
+	void RemoveGridPoint(GridPoint *p);
+	bool IsEmpty();
+	std::vector<GridPoint*> GetGridPoints();
+
+	std::vector<GridPoint*> mNeighbours;
+	int i, j, k;
+
+private:
+	std::vector<GridPoint*> mPoints;
+};
