37b0771c2fd8d8b8c0a715d7cf2b2c408c77f0d6
Balakumaran Palanivel <kumaraxe@gmail.com>
2017-12-10 21:08:08 +0000

    [INTEG] Adding Constants for SPH Fluid. Find a better way to store this

--- FluidSimulation/FluidSimulation/src/Configuration.h
+++ FluidSimulation/FluidSimulation/src/Configuration.h
@@ -5,3 +5,21 @@ enum Screen
 	HEIGHT = 720
 };
 
+class SPHFluidSimulation
+{
+public:
+	const float RATIO_OF_SPECIFIC_HEAT	= 1.0;
+	const float PRESSURE_COEFFICIENT	= 20.0;
+	const float INITIAL_DENSITY = 20.0;
+	const float VISCOSITY_COEFFICIENT = 0.018;
+	const float PARTICLE_MASS = 1.0;
+	const float MAXIMUM_VELOCITY = 75.0;
+	const float MAXIMUM_ACCELERATION = 75.0;
+	const float MOTION_DAMPING_COEFFICIENT = 0.0;
+	const float BOUNDARY_DAMPING_COEFFICIENT = 0.6;
+	const float GRAVITY_MAGNITUDE = 9.8;
+	const bool IS_MOTION_DAMPING_ENABLED = true;
+	const bool IS_BOUNDARY_PARTICLES_ENABLED = false;
+	const bool DISPLAY_SIMULATION_CONSOLE_OUTPUT = true;
+};
+
